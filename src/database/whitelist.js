e2acb4e12e16d1d4340b0232d45624630cdf30ed
sailesh10115 <50526710+sailesh10115@users.noreply.github.com>
2020-10-29 10:58:11 -0400

    Add files via upload

--- src/database/whitelist.js
+++ src/database/whitelist.js
@@ -1,1 +1,9 @@
-d
+const mongoose = require('mongoose')
+
+const whitelistSchema = mongoose.Schema({
+    guild: String,
+    user: String,
+    id: String,
+});
+
+module.exports = mongoose.model("whitelist", whitelistSchema);
